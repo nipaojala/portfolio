e9c2060de83ff215da6031f644ee7e1a0b4ccfb8
nipaojala <nipa.ojala@gmail.com>
2024-02-01 21:00:15 +0200

    removed unused files

--- lib/edgestore.ts
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { EdgeStoreRouter } from '@/pages/api/edgestore/[...edgestore]'
-import { createEdgeStoreProvider } from '@edgestore/react'
-
-const { EdgeStoreProvider, useEdgeStore } =
-  createEdgeStoreProvider<EdgeStoreRouter>()
-
-export { EdgeStoreProvider, useEdgeStore }
