dc22cfd415e8f7aa66e32d1df2155ac0c9a36af8
Niilo Ojala <niilo.ojala@op.fi>
2025-01-31 15:25:04 +0200

    made project page simple and not snapping

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
-import React, { useEffect, useRef, useState } from 'react'
-import { RxArrowDown, RxArrowUp } from 'react-icons/rx'
+import React from 'react'
 import useWindowSize from '../../components/utils/useWindowSize'
 import { useMobileBarOpenContext } from '../../components/utils/useMobileBarOpenContext'
 
@@ -33,134 +32,62 @@ const Card: React.FC<{
   alt: string
   src: string
   width: number
-  lastSlide: boolean
-  firstSlide: boolean
-  minHeight?: string
-}> = ({
-  title,
-  url,
-  alt,
-  src,
-  width,
-  firstSlide,
-  lastSlide,
-  minHeight = '100dvh',
-}) => {
+  isLastOne: boolean
+}> = ({ title, url, alt, src, width, isLastOne }) => {
   const { isMobilebarOpen } = useMobileBarOpenContext()
-  const { width: screenWidth } = useWindowSize()
+  const borderWidth = 0.8 * width
   return (
-    <div
-      style={{
-        boxShadow: '0 10px 15px 5px rgba(0,0,0,0.5)',
-        minHeight: minHeight,
-        scrollSnapAlign: 'start',
-        justifyContent: screenWidth < 750 ? 'space-between' : 'center',
-      }}
-      className="rounded-xl text-center flex flex-col"
-    >
-      {screenWidth < 750 && (
+    <div className="flex justify-center flex-col m-5 items-center">
+      <button
+        className={`flex justify-center hover:opacity-50 ${
+          isMobilebarOpen && 'cursor-default'
+        }`}
+        onClick={() => !isMobilebarOpen && window.open(url)}
+      >
+        <Image
+          alt={alt}
+          width={width > 750 ? 600 : 300}
+          height={width > 750 ? 600 : 300}
+          src={src}
+          className="scale rounded-xl shadow-md shadow-slate-200"
+        />
+      </button>
+      <p
+        className={`scale pt-5 pb-5`}
+        style={{ maxWidth: width > 750 ? '600px' : '300px' }}
+      >
+        {title}
+      </p>
+      <a href={url}>
+        <p className="scale p-5 pt-1 hover:text-red text-yellow">{url}</p>
+      </a>
+      {!isLastOne ? (
         <div
-          style={{
-            opacity: firstSlide ? '0%' : '100%',
-            top: width > 700 ? '120px' : '20px',
-            transition: 'opacity 0.2s ease',
-          }}
-          className={`animate-bounce text-[33px] opacity-90 relative pl-3`}
-        >
-          <RxArrowUp />
-        </div>
-      )}
-      <div className="flex justify-center flex-col">
-        <button
-          className={`flex justify-center hover:opacity-50 ${
-            isMobilebarOpen && 'cursor-default'
-          }`}
-          onClick={() => !isMobilebarOpen && window.open(url)}
-        >
-          <Image
-            alt={alt}
-            width={width > 750 ? 600 : 300}
-            height={width > 750 ? 600 : 300}
-            src={src}
-            className="scale rounded-xl shadow-md shadow-slate-200 m-5 max-w-200"
-          />
-        </button>
-        <p className="scale p-5">{title}</p>
-        <a href={url}>
-          <p className="scale p-5 pt-1 hover:text-red text-yellow">{url}</p>
-        </a>
-      </div>
-      {screenWidth < 750 && (
-        <div
-          style={{
-            bottom: '20px',
-            opacity: lastSlide ? '0%' : '100%',
-            transition: 'opacity 0.2s ease',
-          }}
-          className="animate-bounce text-[33px] opacity-90 relative pl-3"
-        >
-          <RxArrowDown />
-        </div>
-      )}
+          style={{ width: borderWidth }}
+          className={` border border-gray-500`}
+        ></div>
+      ) : null}
     </div>
   )
 }
 
 export default function Projects() {
   const { width } = useWindowSize()
-  const containerRef = useRef<HTMLDivElement | null>(null)
-  const [lastSlide, setLastSlide] = useState(false)
-  const [firstSlide, setFirstSlide] = useState(true)
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const container = containerRef.current
-      if (container) {
-        Math.abs(
-          container.scrollHeight - container.clientHeight - container.scrollTop
-        ) < 600
-          ? setLastSlide(true)
-          : setLastSlide(false)
-
-        container.scrollTop < 600 ? setFirstSlide(true) : setFirstSlide(false)
-      }
-    }
-
-    const container = containerRef.current
-    if (container) {
-      container.addEventListener('scroll', handleScroll)
-    }
-
-    return () => {
-      if (container) {
-        container.removeEventListener('scroll', handleScroll)
-      }
-    }
-  }, [])
   return (
     <>
-      <div
-        ref={containerRef}
-        style={{
-          scrollSnapType: 'y mandatory',
-          overflow: 'scroll',
-          height: '100%',
-        }}
-        className={`flex flex-col`}
-      >
+      <div className={`flex flex-col`}>
         {projects.map((project, index) => {
           const { alt, src, url, title } = project
           return (
             <Card
-              lastSlide={lastSlide}
-              firstSlide={firstSlide}
               key={alt}
               src={src}
               alt={alt}
               url={url}
               width={width}
               title={title}
-              minHeight={index === 0 ? '90dvh' : undefined}
+              isLastOne={index === projects.length - 1}
             />
           )
         })}
